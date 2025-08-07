d11bc9789650c87ff288aba194c339af392b4547
Abraham Rodriguez <abrahamr920@gmail.com>
2025-08-07 16:53:11 -0600

    feat: primer commit del proyecto React Backend (importado)

---
+++ src/main/java/com/abraham/react_backend/repositories/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.abraham.react_backend.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.abraham.react_backend.entities.Product;
+
+// @CrossOrigin("*")
+@CrossOrigin(origins = { "http://localhost:5173",
+        "http://localhost:4200" })
+@RepositoryRestResource(path = "products")
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+}
