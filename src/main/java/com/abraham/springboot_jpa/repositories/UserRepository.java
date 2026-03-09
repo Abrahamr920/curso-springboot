00af2cb802608a0bb93a542ce96418c9d68185c7
Abraham Rodriguez <abrahamr920@gmail.com>
2026-03-09 08:55:23 -0600

    mejora de las consultas

--- src/main/java/com/abraham/springboot_jpa/repositories/UserRepository.java
+++ src/main/java/com/abraham/springboot_jpa/repositories/UserRepository.java
@@ -3,16 +3,19 @@ package com.abraham.springboot_jpa.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
 import com.abraham.springboot_jpa.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    // @EntityGraph(attributePaths = { "roles" })
+    @Override
+    @EntityGraph(attributePaths = { "roles" })
     List<User> findAll();
 
     boolean existsByUsername(String username);
 
+    @EntityGraph(attributePaths = { "roles" })
     Optional<User> findByUsername(String username);
 }
